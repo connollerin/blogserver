9fb2af765595c3d337836430cb8788a64ccd213d
connollerin <connollerin@gmail.com>
2016-08-07 13:16:34 -0400

    trying to get through setup, but ony got to testing the curl commands because mongodb isn't really working

--- app/server.js
+++ app/server.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import apiRouter from './router';
 
 // initialize
 const app = express();
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/blog';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
 
 // enable/disable cross origin resource sharing if necessary
 app.use(cors());
@@ -17,6 +25,10 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('hi');
 });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routres will be prefixed with /api
+app.use('/api', apiRouter);
 
 // START THE SERVER
 // =============================================================================
